bdcf87712ad38aa3bdfc0418b977d53ac7a3abcf
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-18 09:11:15 -0400

    added Head.js

--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+const head = array => array[0];
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,3 +7,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]));
