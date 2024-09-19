e5c272ef8810eb1de711ced7f59d9df0e01feb53
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-19 10:13:13 -0400

    another edit to assertArraysEqual.js to add console.assert

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,9 +12,9 @@ return true;
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅✅ Assertion Passed: ${array1} === ${array2}`);
+    console.assert(`✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    console.assert(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
