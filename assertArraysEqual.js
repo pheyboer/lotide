a2d25ce2221d2970a57a7e06d22a185b88173f89
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-19 09:59:41 -0400

    added assertArraysEqual.js again

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
 if (array1.length !== array2.length) {
   return false;
