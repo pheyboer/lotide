1aa7a693602162afc1466719c283696345406c37
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-19 09:28:45 -0400

    Implement eqArrays with test cases, remved console log

--- eqArrays.js
+++ eqArrays.js
@@ -17,8 +17,6 @@ for (let i = 0; i < array1.length; i++) {
 }
 return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
