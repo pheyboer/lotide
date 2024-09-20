2eb58a858c81c68c80b609a0f95c41662867cead
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-20 10:54:30 -0400

    added assertObjectsEqual with test cases and used inspect in assertion

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -39,9 +39,9 @@ const eqObjects = function(object1, object2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
