0db24362b13a26f68708f424d00dde8cf70668a6
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-22 14:36:12 -0400

    linting

--- eqArrays.js
+++ eqArrays.js
@@ -7,15 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-if (array1.length !== array2.length) {
-  return false;
-}
-for (let i = 0; i < array1.length; i++) {
-  if (array1[i] !== array2[i]) {
+  if (array1.length !== array2.length) {
     return false;
   }
-}
-return true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
