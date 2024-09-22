86521647567d255ea62778e288a8773a8172378d
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-22 14:25:53 -0400

    linting and cleaning

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -12,11 +12,11 @@ const findKeyByValue = function(obj, val) {
       return key;
     }
   }
-  return undefined
+  return undefined;
 };
 
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  sci_Fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
@@ -30,10 +30,10 @@ console.log(findKeyByValue({}, "anything"));
 
 
 
-// Implement the function findKeyByValue which takes in an object and a value. 
-// It should scan the object and return 
-// the first key which contains the given value. 
-// If no key with that given value is found, then it should return undefined.
+// Implement the function findKeyByValue which takes in an object and a value.
+// It should scan the object and return
+// the first key which contains the given value.
+// If no key with that given value is found, then it should return undefined
 //use object.keys
 //use for...of to loop over the keys returned by Object.kesy
 
