2276f4bde8740546f7e468f78a775e103d370896
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-22 14:02:17 -0400

    added takeUntil for lotide with notes and tests

---
+++ takeUntil.js
@@ -0,0 +1,66 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  };
+  
+  const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1, array2)) {
+      console.log(`✅✅ Assertion Passed: ${array1} === ${array2}`);
+    } else {
+      console.log(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }
+  };
+
+//eqArrays and assertArraysEqual functions above
+//new takeUntil function below with assertions
+
+
+const takeUntil = function(array, callback) {
+  const result = [];
+
+  for (const item of array) {
+    if (callback(item)) {
+      break; //this will STOP if the callback returns a truthy value
+    }
+    result.push(item);
+  }
+  return result;
+}
+// created an empty array to store items
+// iterate through each item of input array
+// check if callback returns truthy value -if it does exit, if it doesnt push item into the new result array
+//return result
+
+//test cases using data1 results 1 examples
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results1, [1, 2, 5]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood", ",", "been"]);
+
+
+
+
+
+
+//takeUntil will take in 2 parameters
+// the array to work with 
+//and the callback (lodash calls this "predicate")
+//callback should only be provided one value until returns truthy value
+//because takeUntil returns arrays use assertArraysEqual to compare its return value against unexpected array
