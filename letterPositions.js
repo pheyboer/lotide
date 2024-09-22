7fa69c08c6aae1c589df0c45c3cc6afb2f596453
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-22 14:35:52 -0400

    linting

--- letterPositions.js
+++ letterPositions.js
@@ -8,31 +8,31 @@ const eqArrays = function(array1, array2) {
     }
   }
   return true;
-  };
+};
   
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅✅ Assertion Passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    }
-  };
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
 
 
 
-const letterPositions = function (sentence) {
+const letterPositions = function(sentence) {
   const results = {};
   //iterate over the sentence
   for (let i = 0; i < sentence.length; i++) {
     //skip spaces
-    const letter = sentence[i]
+    const letter = sentence[i];
     if (letter !== " ") {
       // if letter reappears - update the existing array, add new index to the array
       if (letter in results) {
-        results[letter].push(i)
+        results[letter].push(i);
       } else {
         // if letter is new to the result object add new key - and add index to array value
-        results[letter] = [i]
+        results[letter] = [i];
       }
     }
   }
@@ -46,7 +46,7 @@ assertArraysEqual(letterPositions("hello").e, [1]);
 //define object structure to return
 //letterPositions will return all the indices in the string where each character is found - counting letter positions
 //results in an object that maps each letter in the string to an array of its indices
-//write a test with a small string "hello" - 
+//write a test with a small string "hello" -
 //we want to see the string hello laid out telling us at which index each letter is
 //return object keys are letters.
 //values are indices
