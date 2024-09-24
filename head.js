15ea49bf3e53310f60d8afdbe0a0b05d585877e8
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-23 21:08:34 -0400

    took out unnecessary comment

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
 const head = array => array[0]; //head function recieves first element of an array
 
 module.exports = head;
-
-//head function takes an array as an argument and returns first element array[0]
