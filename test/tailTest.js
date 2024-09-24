f459c561ed8f6964c2f0516df48c5bcf36e19bd3
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-23 23:53:26 -0400

    added refactored tailTest.js with notes

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,36 @@
-const assertEqual = require("../assertEqual");
+const { assert } = require("chai");
 const tail = require("../tail");
 
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
-//test with multiple elements
+  it("should return the right length after removing first element", () => {
+    assert.strictEqual(tail(words).length, 2); // checks that length of array returned is 2. first element removed
+  });
 
-assertEqual(tail(words).length, 2); //tail function removes first element
-assertEqual(tail(words)[0], "Lighthouse"); //check the first and second elements
-assertEqual(tail(words)[1], "Labs");
+  it("should return 'Lighthouse' as the first element", () => {
+    assert.strictEqual(tail(words)[0], "Lighthouse"); //return first element lighthouse check
+  });
 
-const singleElementArray = ["Only One"];
-//test with 1 element
-assertEqual(singleElementArray.length, 1);
-assertEqual(tail(singleElementArray).length, 0);
+  it("should return 'Labs' as second element", () => {
+    assert.strictEqual(tail(words)[1], "Labs");//return second element labs check
+  });
 
-const emptyArray = [];
-//test with empty array
-assertEqual(emptyArray.length, 0);
-assertEqual(tail(emptyArray).length, 0);
+  const singleElementArray = ["Only One"]; //set up a variable with one element to test
 
+  it("should return empty array when given single element array", () => {
+    assert.strictEqual(singleElementArray. length, 1);
+    assert.strictEqual(tail(singleElementArray).length, 0);
+  });
+
+  const emptyArray = []; // set up an empty array to test for empty array
+
+  it("should return empty array when given empty array", () => {
+    assert.strictEqual(emptyArray.length, 0);
+    assert.strictEqual(tail(emptyArray).length, 0);
+  });
+});
 //removed intermediate variables
+//refactored with mocha using describe it blocks above
+//test with multiple elements
+//
