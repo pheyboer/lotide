870548149edf4f69c31b7a8aa9ae57600e33df52
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-23 21:05:20 -0400

    added assertEqualTest.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("yes", "yes");
+assertEqual("yes", "no");
+assertEqual(3, 2);
