f9f80436f7cc29ddf42778e2cfb417db66ad37bb
Peter Heyboer <heyboer.peter@gmail.com>
2024-09-23 22:06:41 -0400

    refactored middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   const length = array.length;
   if (length < 3) {
@@ -33,11 +13,4 @@ const middle = function(array) {
   }
 };
 
-const arrayLessThanThree = [1, 2];
-assertArraysEqual(middle(arrayLessThanThree), []);
-
-const evenArray = [1, 2, 3, 4];
-assertArraysEqual(middle(evenArray), [2, 3]);
-
-const oddArray =  [1, 2, 3, 4, 5];
-assertArraysEqual(middle(oddArray), [3]);
+module.exports = middle;
