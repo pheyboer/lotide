b4a27d2755921301dd6da3f181cb8c1f7a611038
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-10 13:38:24 -0400

    added countOnlyTest.js

---
+++ test/countOnlyTest.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const countOnly = require("../countOnly");
+
+describe('#countOnly', () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe",
+  ];
+
+  it("should return counts of items", () => {
+    const result1 = countOnly(firstNames, {
+      Jason: true,
+      Karima: true,
+      Fang: true,
+      Agouhanna: false,
+    });
+
+    assert.equal(result1["Jason"], 1);
+    assert.equal(result1["Karima"], undefined);
+    assert.equal(result1["Fang"], 2);
+    assert.equal(result1["Agouhanna"], undefined);
+  });
+});
