76d6fe0679265fb7962cf25c9c4ffed27cf92cc2
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-10 13:38:51 -0400

    added eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,24 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe('#eqArrays', () => {
+  it("should return true for arrays that are the same", () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("should return false for arrays of different lengths", () => {
+    assert.isFalse(eqArrays([1, 2, 3], [1, 2,]), false);
+  });
+
+  it("should return false for arrays out of order", () => {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("should return true for arrays that have identical strings", () => {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it("should return false for arrays with mixed types", () => {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+});
