886a6976114c7b37bef0f9d4abffff7f90d85646
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-09 20:13:25 -0400

    editing countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -20,7 +20,7 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return results;
 };
-
+//Function to count only specific items from an array and return count of each item
 
 const firstNames = [
   "Karl",
